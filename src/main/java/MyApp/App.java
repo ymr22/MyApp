26312267019e0597567b7c54d4f6bab7eb53c785
Yagmur <yagmurustel@gmail.com>
2022-03-17 20:52:23 +0300

    I've started working on my app.

--- src/main/java/MyApp/App.java
+++ src/main/java/MyApp/App.java
@@ -3,6 +3,8 @@
  */
 package MyApp;
 
+import java.util.ArrayList;
+
 public class App {
     public String getGreeting() {
         return "Hello world.";
@@ -11,4 +13,14 @@ public class App {
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
     }
+
+    public static boolean search(ArrayList<Integer> array, int e) {
+        System.out.println("inside search");
+        if (array == null) return false;
+        for (int elt : array) {
+            if (elt == e) return true;
+        }
+        return false;
+    }
+    
 }
